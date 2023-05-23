c250120b3dbf5d531e0d16c2895635a669e1fc26
axewhyzed <coding0234@gmail.com>
2023-05-23 13:13:20 +0530

    Bookstore Project Started - Registeration Form with Formik and Yup validation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,110 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import { TextField, Button, Typography } from '@material-ui/core';
 
-function App() {
+const validationSchema = Yup.object().shape({
+  name: Yup.string().required('Name is required'),
+  email: Yup.string().email('Invalid email').required('Email is required'),
+  password: Yup.string()
+    .min(8, 'Password must be at least 8 characters')
+    .required('Password is required'),
+  confirmPassword: Yup.string()
+    .oneOf([Yup.ref('password'), null], 'Passwords must match')
+    .required('Confirm Password is required'),
+});
+
+const App = () => {
+  const initialValues = {
+    name: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  };
+
+  const handleSubmit = (values) => {
+    console.log(values);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Typography variant="h4" component="h1" align="center">
+        Registration Form
+      </Typography>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={handleSubmit}
+      >
+        <Form className="form">
+          <div className="form-group">
+            <Field
+              as={TextField}
+              type="text"
+              id="name"
+              name="name"
+              label="Name"
+              variant="outlined"
+              fullWidth
+              margin="normal"
+            />
+            <ErrorMessage name="name" component="div" className="error" />
+          </div>
+
+          <div className="form-group">
+            <Field
+              as={TextField}
+              type="email"
+              id="email"
+              name="email"
+              label="Email"
+              variant="outlined"
+              fullWidth
+              margin="normal"
+            />
+            <ErrorMessage name="email" component="div" className="error" />
+          </div>
+
+          <div className="form-group">
+            <Field
+              as={TextField}
+              type="password"
+              id="password"
+              name="password"
+              label="Password"
+              variant="outlined"
+              fullWidth
+              margin="normal"
+            />
+            <ErrorMessage name="password" component="div" className="error" />
+          </div>
+
+          <div className="form-group">
+            <Field
+              as={TextField}
+              type="password"
+              id="confirmPassword"
+              name="confirmPassword"
+              label="Confirm Password"
+              variant="outlined"
+              fullWidth
+              margin="normal"
+            />
+            <ErrorMessage
+              name="confirmPassword"
+              component="div"
+              className="error"
+            />
+          </div>
+
+          <Button type="submit" variant="contained" color="primary">
+            Register
+          </Button>
+        </Form>
+      </Formik>
     </div>
   );
-}
+};
 
 export default App;
