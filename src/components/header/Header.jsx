af96b6e64135d2f33d0ff7702a8de83a86998762
axewhyzed <coding0234@gmail.com>
2023-05-27 19:05:52 +0530

    Added Book Search feature

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,19 +3,74 @@ import { NavLink } from "react-router-dom";
 import "./headerStyles.css";
 import { RoutePaths } from "../../utils/enum";
 import bookService from "../../services/book.service";
-import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useState } from "react";
+import SearchIcon from "@material-ui/icons/Search";
+import { makeStyles } from "@material-ui/core/styles";
+import { Typography, TextField, IconButton, Card, CardContent} from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  searchContainer: {
+    display: "flex",
+    alignItems: "center",
+    marginRight: theme.spacing(2),
+  },
+  searchInput: {
+    color: "#fff",
+  },
+  resultContainer: {
+    position: "absolute",
+    top: "100%",
+    left: 0,
+    width: "100%",
+    maxHeight: "300px",
+    overflowY: "auto",
+    backgroundColor: "#fff",
+    boxShadow: "0px 0px 5px rgba(0, 0, 0, 0.2)",
+    zIndex: 999,
+    padding: theme.spacing(1),
+  },
+  resultCard: {
+    display: "flex",
+    alignItems: "center",
+    marginBottom: theme.spacing(1),
+    padding: theme.spacing(1),
+  },
+  resultImage: {
+    width: 60,
+    height: 90,
+    objectFit: "cover",
+    borderRadius: theme.spacing(1),
+    marginRight: theme.spacing(2),
+  },
+  resultDetails: {
+    flex: 1,
+    marginLeft: theme.spacing(2),
+  },
+  resultName: {
+    fontWeight: "bold",
+    fontSize: 14,
+  },
+  resultDescription: {
+    fontSize: 12,
+    color: theme.palette.text.secondary,
+  },
+  resultPrice: {
+    fontSize: 12,
+    color: theme.palette.text.secondary,
+  },
+}));
 
 const Header = () => {
-  const initialValues = {
-    searchText: "",
-  };
+  const classes = useStyles();
+  const [searchText, setSearchText] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
 
-  const handleSearch = async (values) => {
+  const handleSearch = async () => {
     try {
-      console.log("Submitted:", values);
-      bookService.search(values).then((res) => {
-        toast.success("Successfully Searched");
-      });
+      const result = await bookService.searchBook(searchText);
+      console.log("Submitted:", result);
+      setSearchResults(result);
     } catch (error) {
       console.log(error);
       // Handle any errors that occur during the search
@@ -34,6 +89,46 @@ const Header = () => {
           Register
         </NavLink>
       </nav>
+      <div className={classes.searchContainer}>
+        <TextField
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
+          placeholder="Search Books"
+          InputProps={{
+            className: classes.searchInput,
+          }}
+        />
+        <IconButton color="inherit" onClick={handleSearch}>
+          <SearchIcon />
+        </IconButton>
+      </div>
+      {searchResults.length > 0 && (
+        <div className={classes.resultsContainer}>
+        <Typography variant="h6">Search Results:</Typography>
+        <div className={classes.resultsList}>
+          {searchResults.map((result) => (
+            <Card key={result.id} className={classes.resultCard}>
+              <img
+                src={result.base64image}
+                alt="book-pic"
+                className={classes.resultImage}
+              />
+              <CardContent className={classes.resultDetails}>
+                <Typography variant="h6" className={classes.resultName}>
+                  {result.name}
+                </Typography>
+                <Typography variant="body2" className={classes.resultDescription}>
+                  {result.description}
+                </Typography>
+                <Typography variant="body2" className={classes.resultPrice}>
+                  Price: {result.price}
+                </Typography>
+              </CardContent>
+            </Card>
+          ))}
+        </div>
+      </div>
+      )}
     </header>
   );
 };
